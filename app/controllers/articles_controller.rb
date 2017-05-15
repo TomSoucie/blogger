f0ecf37dd20c98b934ab25551733d094e14521b7
Thomas Soucie <tsoucie1858@gmail.com>
2017-05-15 17:00:49 -0600

    Commits edit view and partials implementation for new and edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:destroy, :edit]
+  before_action :set_article, only: [:destroy, :edit, :update]
   def index
     @articles = Article.all
   end
@@ -19,16 +19,18 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
 
     redirect_to articles_path
   end
 
   def edit
-    @article = Article.find(params[:id])
+  end
 
+  def update
+    @article.update(article_params)
 
+    redirect_to article_path(@article)
   end
 
 
