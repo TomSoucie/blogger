f8b89af558ec96148b44dbf51a19e35181b817c4
Thomas Soucie <tsoucie1858@gmail.com>
2017-05-15 12:46:12 -0600

    Commits delete function through articles_controller and show view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,13 +14,21 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
-    
+
     redirect_to article_path(@article)
   end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
 
   private
 
   def article_params
     params.require(:article).permit(:title, :body)
   end
+
 end
