4cb990e012fb85b5719aa0b0d4c00529747362bf
Thomas Soucie <tsoucie1858@gmail.com>
2018-07-26 10:15:22 -0600

    Commits untracked changes 26Jul2018

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -49,7 +49,7 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :body)
+    params.require(:article).permit(:title, :body, :tag_list)
   end
 
 end
